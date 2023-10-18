873cfc6566c245680245b08fe2af00c8ecfedf48
V <you@example.com>
2023-10-18 11:16:56 +0200

    update

--- src/Constants.ts
+++ src/Constants.ts
@@ -3,6 +3,10 @@ export const horizontalAxis = ['a', 'b', 'c', 'd', 'e'];
 
 export const gridSize = 200;
 
+export function samePosition(p1: Position, p2 : Position){
+    return p1.x === p2.x && p2.y === p1.y;
+}
+
 export interface Position{
     x: number;
     y: number;
@@ -23,24 +27,29 @@ export interface Piece {
     position: Position;
     type: PieceType;
     color: PieceColor;
+    possibleMoves?: Position[];
 };
 
+
+
+export const sun = "kings_valley/assets/images/sun.png";
+
 export const initialBoardState: Piece[] = [
-{image: "assets/images/white-pawn.png", position:{x: 0, y: 0}, type: PieceType.PAWN, color: PieceColor.WHITE},
-{image: "assets/images/white-pawn.png", position:{x: 1, y: 0}, type: PieceType.PAWN, color: PieceColor.WHITE},
+{image: "kings_valley/assets/images/white-pawn.png", position:{x: 0, y: 0}, type: PieceType.PAWN, color: PieceColor.WHITE},
+{image: "kings_valley/assets/images/white-pawn.png", position:{x: 1, y: 0}, type: PieceType.PAWN, color: PieceColor.WHITE},
 
-{image: "assets/images/white-king.png", position:{x: 2, y: 0}, type: PieceType.KING, color: PieceColor.WHITE},
+{image: "kings_valley/assets/images/white-king.png", position:{x: 2, y: 0}, type: PieceType.KING, color: PieceColor.WHITE},
 
-{image: "assets/images/white-pawn.png", position:{x: 3, y: 0}, type: PieceType.PAWN, color: PieceColor.WHITE},
-{image: "assets/images/white-pawn.png", position:{x: 4, y: 0}, type: PieceType.PAWN, color: PieceColor.WHITE},
+{image: "kings_valley/assets/images/white-pawn.png", position:{x: 3, y: 0}, type: PieceType.PAWN, color: PieceColor.WHITE},
+{image: "kings_valley/assets/images/white-pawn.png", position:{x: 4, y: 0}, type: PieceType.PAWN, color: PieceColor.WHITE},
 
 
 
-{image: "assets/images/black-pawn.png", position:{x: 0, y: 4}, type: PieceType.PAWN, color: PieceColor.BLACK},
-{image: "assets/images/black-pawn.png", position:{x: 1, y: 4}, type: PieceType.PAWN, color: PieceColor.BLACK},
+{image: "kings_valley/assets/images/black-pawn.png", position:{x: 0, y: 4}, type: PieceType.PAWN, color: PieceColor.BLACK},
+{image: "kings_valley/assets/images/black-pawn.png", position:{x: 1, y: 4}, type: PieceType.PAWN, color: PieceColor.BLACK},
 
-{image: "assets/images/black-king.png", position:{x: 2, y: 4}, type: PieceType.KING, color: PieceColor.BLACK},
+{image: "kings_valley/assets/images/black-king.png", position:{x: 2, y: 4}, type: PieceType.KING, color: PieceColor.BLACK},
 
-{image: "assets/images/black-pawn.png", position:{x: 3, y: 4}, type: PieceType.PAWN, color: PieceColor.BLACK},
-{image: "assets/images/black-pawn.png", position:{x: 4, y: 4}, type: PieceType.PAWN, color: PieceColor.BLACK}
+{image: "kings_valley/assets/images/black-pawn.png", position:{x: 3, y: 4}, type: PieceType.PAWN, color: PieceColor.BLACK},
+{image: "kings_valley/assets/images/black-pawn.png", position:{x: 4, y: 4}, type: PieceType.PAWN, color: PieceColor.BLACK}
 ];
